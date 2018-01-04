d0aa42e6cbe26f2960a32a382f0caf045ff53076
spring96ruki <jiru_spring@yahoo.co.jp>
2018-01-05 03:45:46 +0900

    あけおめことよろ

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public Map m_map;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private IEnumerator Start()
+    {
+        m_map.MapGenerate(10,10);
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/Map_cell.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map_cell : MonoBehaviour {
+
+    public Map m_map;
+
+    [SerializeField]
+    int m_cost;
+
+    int m_x;
+    public int X {
+        get {
+            return m_x;
+        }
+    }
+
+    int m_y;
+    public int Y {
+        get {
+            return m_y;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void setPoint(int x, int y) {
+        m_x = x;
+        m_y = y;
+        Vector2 cellPos = transform.position;
+        cellPos.x = m_x;
+        cellPos.y = m_y;
+        transform.position = new Vector2(cellPos.x, cellPos.y);
+    }
+}
